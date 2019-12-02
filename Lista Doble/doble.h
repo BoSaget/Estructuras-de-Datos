d63104cdf618ea6710dd3f3c144d3079a76e43ab
BoSaget <47957644+BoSaget@users.noreply.github.com>
2019-12-02 02:37:57 -0600

    Add files via upload

--- Lista Doble/doble.h
+++ Lista Doble/doble.h
@@ -3,13 +3,13 @@
 
 struct Doble
 {
-	int dato;
-	struct Doble * Siguiente;
-	struct Doble * Anterior;	
+  int dato;
+  struct Doble *Siguiente;
+  struct Doble *Anterior;
 };
 
-struct Doble * Insertar_Nodo(struct Doble * , int);
+struct Doble *Insertar_Nodo (struct Doble *, int);
 
-void Mostrar_ListaDoble(struct Doble *);
+void Mostrar_ListaDoble (struct Doble *);
 
-struct Doble * Eliminar_Nodo(struct  Doble * , int);
+struct Doble *Eliminar_Nodo (struct Doble *, int);
--- Lista Doble/main.c
+++ Lista Doble/main.c
@@ -1,24 +1,26 @@
 #include "doble.h"
 
-int main (void)
+int
+main (void)
 {
-	struct Doble * Numeros;
-	Numeros=NULL;
-	
-	Numeros=Insertar_Nodo(Numeros, 5);
-	Numeros=Insertar_Nodo(Numeros, 4);
-	Numeros=Insertar_Nodo(Numeros, 3);
-	Numeros=Insertar_Nodo(Numeros, 2);
-	Numeros=Insertar_Nodo(Numeros, 1);
+  struct Doble *Numeros;
+  Numeros = NULL;
 
-	Mostrar_ListaDoble(Numeros);
-	
-	
-	printf("\n");
-	
-	Numeros=Eliminar_Nodo(Numeros, 2);
-	
-	Mostrar_ListaDoble(Numeros);
-	
-return 0;
+  Numeros = Insertar_Nodo (Numeros, 5);
+  Numeros = Insertar_Nodo (Numeros, 4);
+
+  Numeros = Insertar_Nodo (Numeros, 3);
+  Numeros = Insertar_Nodo (Numeros, 2);
+  Numeros = Insertar_Nodo (Numeros, 1);
+
+  Mostrar_ListaDoble (Numeros);
+
+
+  printf ("\n");
+
+  Numeros = Eliminar_Nodo (Numeros, 2);
+
+  Mostrar_ListaDoble (Numeros);
+
+  return 0;
 }
